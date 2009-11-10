749dcba15f4c92ed2528b3d6a20ce01e1093ec4d
Lauri <Lauri@88.115.179.185>
2009-11-11 01:31:50 +0200

    Lisätty application helpperiin sivun luomisen ajan näyttäminen suomalaisittain ja lisätty sen näyttäminen yleis layouttiin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+    def luontiaika
+    a = Time.now
+    aika = ""+a.day.to_s + "." + a.month.to_s + "." + a.year.to_s+" " +a.hour.to_s+":" + a.min.to_s
+    return aika
+  end
 end
