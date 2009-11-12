72e129ab2918ab8d557db6635bd306ad227aae31
Your Name <lauri.sivula@gmail.com>
2009-11-12 10:58:11 +0200

    Muokattu actionhelpperin sivun luomisajan generointia suomalaisittain siten että että esim minuutin yli yhdeksän näytetään 09:01 eikä 9:1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@
 module ApplicationHelper
     def luontiaika
     a = Time.now
-    aika = ""+a.day.to_s + "." + a.month.to_s + "." + a.year.to_s+" " +a.hour.to_s+":" + a.min.to_s
+    hour = a.hour.to_s
+    min = a.min.to_s
+    if a.hour < 10
+      hour = "0"+hour
+      end
+    if a.min <10
+      min = "0"+min
+    end
+    aika = ""+a.day.to_s + "." + a.month.to_s + "." + a.year.to_s+" " +hour+":" + min
     return aika
   end
 end
