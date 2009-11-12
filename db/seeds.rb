7968c78171be53062cc560099e5bd9448c029f6c
Lauri <lauri.sivula@gmail.com>
2009-11-12 11:51:58 +0200

    Muokattu seeds.rb tiedostoa siten että esim komennolla "rake db:seed maara=10 poistetaan=1" scripti generoi 10 uuta kurssia ja poistaa kaiki vanhat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+if ENV['maara']==nil or ENV['poistetaan']==nil
+puts "Kaytto: rake db:seed maara=XXX poistetaan=X \n"
+puts "maara: XXX: Kuinka monta kurssia lisataan \n"
+puts poistetaan" 1: Poistetaan vanhat kurssit ta 0: sailytetaan vanhat kurssit"
+puts "esim  rake db:seed maara=10 poistetaan=1        poistaa kaikki vanhat kurssit ja lisää 10 uutta "
+return
+end
+if ENV['poistetaan']=="1"
+  puts "Poistetaan kaikki aikaisemmat kurssit"
+  Course.destroy_all
+end
+  i=0
+  puts "Lisataan "+ ENV['maara']+ " kurssia"
+  ENV['maara'].to_i.times{
+    i=i+1    
+  Course.create(:name=>"Kurssi "+i.to_s, :description=> "Kurssin "+i.to_s+" kuvaus selityksineen")
+  }
