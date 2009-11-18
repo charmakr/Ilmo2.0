de564bab503841dba83598aca770619610c69f67
Lauri <lauri.sivula@gmail.com>
2009-11-18 20:55:26 +0200

    Muokattu sessions kontrollerin create a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   skip_before_filter :authentication_required
+  def index    
+end
+
   def create
+    session[:user]=true
+    redirect_to "/"
   end
 end
