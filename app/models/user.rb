0ee8092fdc49e602ac1d12ac7dc1a5866c3603d9
Lauri <lauri.sivula@gmail.com>
2009-11-18 22:55:10 +0200

    Lisätty User modeliin rekisteröinti

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ActiveRecord::Base
     
     return u
   end
+  def self.register(username, password)
+        u = User.find(:first, :conditions=>["username = ?", username])        
+    if u==nil
+      User.create(:username=>username, :password=>password)
+      flash[:sucsess]="Käyttäjän luonti onnistui"
+      return
+    end
+    flash[:warning]="Käyttäjänimi on jo käytössä"
+  end
+    
+    
 end
