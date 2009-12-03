91a1c57fbf0f49f244675382a2a16daab426f77f
Lauri <lauri.sivula@gmail.com>
2009-12-03 15:43:44 +0200

    Lisätty rss builder newsfeedille ja linkki etusivulle

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,10 @@ class CoursesController < ApplicationController
   def index
     @newsfeed = Newsfeed.all
     @courses = Course.down
+    respond_to do |format|
+      format.html
+      format.rss
+    end
   end
   def show
     @courses = Course.find(params[:id])   
