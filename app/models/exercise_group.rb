cf09d0693277eccd2ac5a067735b69118e46d3dd
Lauri <lauri.sivula@gmail.com>
2009-12-03 19:25:49 +0200

    Parannettu tarkistusta exercise_group lukumäärästä

--- app/models/exercise_group.rb
+++ app/models/exercise_group.rb
@@ -7,9 +7,14 @@ class ExerciseGroup < ActiveRecord::Base
   validate :check
   
   def check
-    if(self.course_instance.max<=self.course_instance.exercise_groups.size) 
-     errors.add_to_base("Liikaa ryhmiä")
+    if(self.course_instance.multiple_exercise_groups)
+      if(self.course_instance.max<=self.course_instance.exercise_groups.size) 
+        errors.add_to_base("Liikaa ryhmiä")
+      end
+      return
     end
+    if(self.course_instance.exercise_groups.size!=0)
+      errors.add_to_base("Liikaa ryhmiä")
   end
   
   
