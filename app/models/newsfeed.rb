fdc295e2a3f562374cc6c066c9d1da9208c426b1
Lauri <lauri.sivula@gmail.com>
2009-12-03 14:25:58 +0200

    Muokattu Newsfeed siten että harjoitusryhmään liittyminen näkyy siinä

--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -7,4 +7,7 @@ class Newsfeed < ActiveRecord::Base
     def self.course_added(course)
     self.create :message => "Uusi kurssi "+ course.name + " lisättiin"
   end
+  def self.join_exercise_group(user, exg)
+    self.create :message => "Käyttäjä "+ user.username + " liittyi kurssin: "+ exg.course_instance.course.name + ":" + exg.course_instance.name + " harjoitusryhmään:" + exg.name 
+  end
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,4 +3,9 @@ class Registration < ActiveRecord::Base
   belongs_to :user
   belongs_to :exercise_group
 
+  after_create :update_newsfeed
+  
+  def update_newsfeed
+    Newsfeed.join_exercise_group(self.user,self.exercise_group )
+  end
 end
