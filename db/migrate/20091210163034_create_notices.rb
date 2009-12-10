0364d1c2f9e040c0622d48200a2f4c7f7ebd4a50
Lauri <lauri.sivula@gmail.com>
2009-12-10 18:33:50 +0200

    Luotu Notice model

---
+++ db/migrate/20091210163034_create_notices.rb
@@ -0,0 +1,13 @@
+class CreateNotices < ActiveRecord::Migration
+  def self.up
+    create_table :notices do |t|
+      t.string    :message
+      t.integer   :user_id
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :notices
+  end
+end
