afa93b79f8996ae3768a0c91610e9cfa941c49be
Lauri <lauri.sivula@gmail.com>
2009-12-10 19:41:38 +0200

    Lisätty linkkejä newsfeediin

--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -6,9 +6,9 @@ class Newsfeed < ActiveRecord::Base
     self.create :message => "Käyttäjä "+ user.username + " rekisteröityi järjestelmään"
   end
     def self.course_added(course)
-    self.create :message => "Uusi kurssi "+ course.name + " lisättiin"
+    self.create :message => "Uusi kurssi" +"<a href=\"courses\\" + course.id.to_s + "\">" + course.name + "</a>" + "lisättiin"
   end
   def self.join_exercise_group(user, exg)
-    self.create :message => "Käyttäjä "+ user.username + " liittyi kurssin: "+ exg.course_instance.course.name + ":" + exg.course_instance.name + " harjoitusryhmään:" + exg.name 
+    self.create :message => "Käyttäjä "+ user.username + " liittyi kurssin: "+ exg.course_instance.course.name + ":" + exg.course_instance.name + " harjoitusryhmään:"  +"<a href=\"courses\\1\\course_instances\\1\\exercise_groups\\" + exg.id.to_s + "\">" + exg.name + "</a>" 
   end  
 end
