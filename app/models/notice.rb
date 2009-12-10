a80eb06e9ea93e94f1ce3b2209b984674b079edc
Lauri <lauri.sivula@gmail.com>
2009-12-10 20:19:35 +0200

    Ilmoitukseen liitetyn käyttäjän tarkistus poistettu

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,13 +1,5 @@
 class Notice < ActiveRecord::Base
   belongs_to :user
   
-  validate_on_create :check_user
-  
-  
-  def check_user
-    if (self.user.account_type!="Admin")
-      errors.add("Admin required")
-      return false
-    end  
-  end
+    
 end
