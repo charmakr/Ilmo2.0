c22ffe7889f7c2093cb26fc3c8375964a126b591
Lauri <lauri.sivula@gmail.com>
2009-12-11 00:01:29 +0200

    Näytetään Kokeeseen osallistujat kokeen sivuilla ja lisätty osallistumis mekanismi kokeeseen

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -4,4 +4,17 @@ class ExamsController < ApplicationController
     @exam = Exam.find_by_id(params[:id])
     @u = @exam.users
   end
+  
+  def join
+    if params[:id]
+      begin
+        Exam.find_by_id(params[:id]).take_part(User.find_by_id(session[:user]))
+      rescue 
+        flash[:warning]="Ongelma"
+        redirect_to course_exam_path(Exam.find_by_id(params[:id]).course,Exam.find_by_id(params[:id]))  and return
+      end
+    end
+    redirect_to course_exam_path(Exam.find_by_id(params[:id]).course,Exam.find_by_id(params[:id]))
+  end
+ 
 end
--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,4 +2,11 @@ class Exam < ActiveRecord::Base
   belongs_to :course
   has_many :exam_registrations, :dependent => :destroy
   has_many :users, :through => :exam_registrations
+  
+  def take_part(user)
+    if self.users.find_by_id(user.id)
+      return
+    end
+    self.users<<user
+  end
 end
